37f1a8fbc63ee1a7b5deb0f0317addc6e2dbca1a
Nidaime-chan <104910191+megashinyabsol359@users.noreply.github.com>
2023-12-11 20:16:33 +0700

    Update __init__.py

--- code/project/__init__.py
+++ code/project/__init__.py
@@ -3,60 +3,11 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_uploads import UploadSet, IMAGES, configure_uploads
 
-# from flask_socketio import SocketIO, emit
-# import io
-# from io import StringIO
-# import cv2
-# from PIL import Image
-# import base64
-# import imutils
-# import numpy as np
-
-# from flask_talisman import Talisman
-
-
-
-
 # init SQLAlchemy so we can use it later in our models
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    
-    # ###
-    
-    # Talisman(app)
-    
-    # socketio = SocketIO(app)
-    
-    # @socketio.on('image')
-    # def image(data_image):
-    #     sbuf = StringIO()
-    #     sbuf.write(data_image)
-
-    #     # decode and convert into image
-    #     b = io.BytesIO(base64.b64decode(data_image))
-    #     pimg = Image.open(b)
-
-    #     ## converting RGB to BGR, as opencv standards
-    #     frame = cv2.cvtColor(np.array(pimg), cv2.COLOR_RGB2BGR)
-
-    #     # Process the image frame
-    #     frame = imutils.resize(frame, width=700)
-    #     frame = cv2.flip(frame, 1)
-    #     imgencode = cv2.imencode('.jpg', frame)[1]
-
-    #     # base64 encode
-    #     stringData = base64.b64encode(imgencode).decode('utf-8')
-    #     b64_src = 'data:image/jpg;base64,'
-    #     stringData = b64_src + stringData
-
-    #     # emit the frame back
-    #     emit('response_back', stringData)
-    # socketio.init_app(app)
-    # ########
-
-
     app.config['SECRET_KEY'] = 'secret-key-goes-here'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
     
