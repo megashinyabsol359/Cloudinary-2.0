bb1f66f1e9e8ea1733dc791aab55baece2b8114f
Nidaime-chan <104910191+megashinyabsol359@users.noreply.github.com>
2023-12-19 15:45:16 +0700

    Add files via upload

---
+++ code/code/project/auth.py
@@ -0,0 +1,270 @@
+from flask import Blueprint, render_template, redirect, url_for, request, flash
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, login_required, logout_user, current_user
+
+import face_recognition
+import cv2
+
+from .models import User
+from . import db
+
+import base64
+import numpy as np
+
+auth = Blueprint('auth', __name__)
+
+@auth.route('/login')
+def login():
+    return render_template('login.html')
+
+@auth.route('/login', methods=['POST'])
+def login_post():
+    # code login
+    email = request.form.get('email')
+    password = request.form.get('password')
+    remember = True if request.form.get('remember') else False
+
+    user = User.query.filter_by(email=email).first()
+
+    # kiểm tra user tồn tại trong database
+    # kiểm tra password với hash
+    if not user or not check_password_hash(user.password, password):
+        flash('Vui lòng kiểm tra lại thông tin đăng nhập')
+        return redirect(url_for('auth.login')) # if the user doesn't exist or password is wrong, reload the page
+
+    # nếu đúng hết ở trên thì đăng nhập người dùng
+    login_user(user, remember=remember)
+    return redirect(url_for('main.profile'))
+
+
+@auth.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@auth.route('/signup', methods=['POST'])
+def signup_post():
+    # code lấy thông tin từ người dùng
+    email = request.form.get('email')
+    name = request.form.get('name')
+    password = request.form.get('password')
+    password_confirm = request.form.get('password_confirm')
+
+    user = User.query.filter_by(email=email).first() # kiểm tra người dùng trong database qua email
+
+    if user: # nếu tìm ra người dùng thì quay về trang signup dùng gmail khác
+        flash('Tài khoản sử dụng Email này đã tồn tại')
+        return redirect(url_for('auth.signup'))
+    if len(password) < 8 or len(password) > 50:# nếu password ko hợp lệ
+        flash('Mật khẩu phải có từ 8 đến 50 ký tự')
+        return redirect(url_for('auth.signup'))
+    if password != password_confirm:# nếu password và nhập lại ko khớp
+        flash('Mật khẩu và xác nhận mật khẩu không khớp')
+        return redirect(url_for('auth.signup'))
+
+    # nếu đúng hết thì tạo tài khoản mới, hash password
+    new_user = User(email=email, name=name, password=generate_password_hash(password))
+
+    # thêm vào database
+    db.session.add(new_user)
+    db.session.commit()
+
+    return redirect(url_for('auth.login'))
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('main.index'))
+
+@auth.route('/register_face')
+@login_required
+def register_face():
+    return render_template('register_face.html')
+
+@auth.route('/register_face', methods=['POST'])
+@login_required
+def register_face_post():
+    user = User.query.filter_by(id=current_user.get_id()).first()
+    password = request.form.get('password')
+    
+    if not check_password_hash(user.password, password):
+        flash('Mật khẩu không đúng!', 'danger')
+        return redirect(url_for('auth.register_face'))
+    
+    uploaded_image = request.files['image']
+
+    if uploaded_image:# kiểm tra có hình chưa
+        image = face_recognition.load_image_file(uploaded_image)
+        face_locations = face_recognition.face_locations(image)
+
+        if len(face_locations) == 1:  # Chỉ xử lý ảnh có một khuôn mặt
+            face_encoding = face_recognition.face_encodings(image)[0]
+
+            # Cắt hình ảnh khuôn mặt
+            top, right, bottom, left = face_locations[0]
+            face_image = image[top:bottom, left:right]
+
+            # Lưu hình ảnh khuôn mặt đã cắt xuống cơ sở dữ liệu
+            user.face_encoding = face_encoding
+            user.face_image = cv2.imencode('.jpg', face_image)[1].tobytes()  # Chuyển hình ảnh thành dữ liệu nhị phân
+
+            db.session.commit()
+            flash('Đăng ký khuôn mặt thành công!', 'success')
+            return redirect(url_for('auth.register_face'))
+        else:
+            flash('Vui lòng tải lên một ảnh chứa duy nhất một khuôn mặt.', 'danger')
+    else:
+        flash('Vui lòng tải lên một ảnh.', 'danger')
+
+    return redirect(url_for('auth.register_face'))
+
+@auth.route('/login_face')
+def login_face():
+    return render_template('login_face.html')
+
+@auth.route('/login_face', methods=['POST'])
+def login_face_post():
+    email = request.form.get('email')
+    user = User.query.filter_by(email=email).first() # kiểm tra tài khoản trong database qua email
+    remember = True if request.form.get('remember') else False
+
+    uploaded_image = request.files['image']
+        
+    if not user: # nếu tìm ra người dùng thì quay về trang signup dùng gmail khác
+        flash('Xác thực bằng khuôn mặt không thành công. Vui lòng kiểm tra lại thông tin đăng nhập.')
+        return redirect(url_for('auth.login_face'))
+
+    if not uploaded_image:# kiểm tra có hình chưa
+        flash('Vui lòng tải lên một ảnh.', 'danger')
+        return redirect(url_for('auth.login_face'))
+        
+    image = face_recognition.load_image_file(uploaded_image)
+    face_locations = face_recognition.face_locations(image)
+
+    if len(face_locations) != 1:
+        flash('Vui lòng tải lên một ảnh chứa duy nhất một khuôn mặt.', 'danger')
+        return redirect(url_for('auth.login_face'))
+    face_encoding = face_recognition.face_encodings(image)[0]
+    
+    if user.face_encoding is None or not face_recognition.compare_faces([user.face_encoding], face_encoding)[0]:
+        flash('Xác thực bằng khuôn mặt không thành công. Vui lòng kiểm tra lại thông tin đăng nhập.', 'danger')
+        return redirect(url_for('auth.login_face'))
+        
+    login_user(user, remember=remember) # Sau khi qua hết thì đăng nhập user
+    return redirect(url_for('main.profile'))  # Chuyển hướng sau khi xác thực bằng khuôn mặt
+
+@auth.route('/register_cam')
+@login_required
+def register_cam():
+    error = request.args.get('error')
+    print(error)
+    return render_template('register_cam.html',error=error)
+
+@auth.route('/register_cam', methods=['POST'])
+@login_required
+def register_cam_post():
+    user = User.query.filter_by(id=current_user.get_id()).first()
+    password = request.json['password']
+    
+    if not check_password_hash(user.password, password):
+        error = 'Mật khẩu không đúng!'
+        return redirect(url_for('auth.register_cam', error=error))
+    
+    # Nhận dữ liệu hình ảnh từ yêu cầu POST
+    data_url = request.json['image']
+
+    # Loại bỏ phần đầu của chuỗi dữ liệu URL (prefix "data:image/jpeg;base64,")
+    img_data = data_url.split(',')[1]
+
+    # Chuyển đổi dữ liệu hình ảnh từ base64 sang binary
+    binary_data = base64.b64decode(img_data)
+
+    jpg_as_np = np.frombuffer(binary_data, dtype=np.uint8)
+    #image_buffer = cv2.imdecode(jpg_as_np, flags=1)
+    
+    image = cv2.imdecode(jpg_as_np, flags=1)
+    face_locations = face_recognition.face_locations(image)
+
+    if len(face_locations) == 1:  # Chỉ xử lý ảnh có một khuôn mặt
+        face_encoding = face_recognition.face_encodings(image)[0]
+
+        # Cắt hình ảnh khuôn mặt
+        top, right, bottom, left = face_locations[0]
+        face_image = image[top:bottom, left:right]
+
+        # Lưu hình ảnh khuôn mặt đã cắt xuống cơ sở dữ liệu
+        user.face_encoding = face_encoding
+        user.face_image = cv2.imencode('.jpg', face_image)[1].tobytes()  # Chuyển hình ảnh thành dữ liệu nhị phân
+
+        db.session.commit()
+        error = 'Đăng ký khuôn mặt thành công!'
+        print('Đăng ký khuôn mặt thành công!')
+        return redirect(url_for('auth.register_cam', error=error))
+    else:
+        error = 'Vui lòng trong facecam chỉ chứa duy nhất một khuôn mặt.'
+        print('Vui lòng trong facecam chỉ chứa duy nhất một khuôn mặt!')
+
+    return redirect(url_for('auth.register_cam', error=error))
+
+
+@auth.route('/login_cam')
+def login_cam():
+    error = request.args.get('error')
+    print(error)
+    return render_template('login_cam.html',error=error)
+
+@auth.route('/login_cam', methods=['POST'])
+def login_cam_post():
+    
+    # Nhận dữ liệu hình ảnh từ yêu cầu POST
+    image_url = request.json['image']
+    
+    email = request.json['email']
+    
+    remember = True if request.json['remember'] else False  
+    
+    
+    # Loại bỏ phần đầu của chuỗi dữ liệu URL (prefix "data:image/jpeg;base64,")
+    img_data = image_url.split(',')[1]
+
+    # Chuyển đổi dữ liệu hình ảnh từ base64 sang binary
+    binary_data = base64.b64decode(img_data)
+
+    jpg_as_np = np.frombuffer(binary_data, dtype=np.uint8)    
+    image = cv2.imdecode(jpg_as_np, flags=1)
+
+
+    user = User.query.filter_by(email=email).first() # kiểm tra tài khoản trong database qua email
+        
+    if not user: # nếu tìm ra người dùng thì quay về trang signup dùng gmail khác
+        flash('Xác thực bằng khuôn mặt không thành công. Vui lòng kiểm tra lại thông tin đăng nhập.')
+        print('Xác thực bằng khuôn mặt không thành công. Vui lòng kiểm tra lại thông tin đăng nhập.')
+        error = 'Xác thực bằng khuôn mặt không thành công. Vui lòng kiểm tra lại thông tin đăng nhập.'
+        return redirect(url_for('auth.login_cam', error=error))
+
+    # if not uploaded_image:# kiểm tra có hình chưa
+    #     flash('Vui lòng tải lên một ảnh.', 'danger')
+    #     print('Vui lòng tải lên một ảnh.')
+    #     return redirect(url_for('auth.login_cam'))
+
+    face_locations = face_recognition.face_locations(image)
+
+    if len(face_locations) != 1:
+        flash('Vui lòng tải lên một ảnh chứa duy nhất một khuôn mặt.', 'danger')
+        print('Vui lòng tải lên một ảnh chứa duy nhất một khuôn mặt.')
+        error = 'Vui lòng tải lên một ảnh chứa duy nhất một khuôn mặt.'
+        return redirect(url_for('auth.login_cam', error=error))
+    face_encoding = face_recognition.face_encodings(image)[0]
+    
+    if user.face_encoding is None or not face_recognition.compare_faces([user.face_encoding], face_encoding)[0]:
+        flash('Xác thực bằng khuôn mặt không thành công. Vui lòng kiểm tra lại thông tin đăng nhập.', 'danger')
+        print('Xác thực bằng khuôn mặt không thành công. Vui lòng kiểm tra lại thông tin đăng nhập.')
+        error = 'Xác thực bằng khuôn mặt không thành công. Vui lòng kiểm tra lại thông tin đăng nhập.'
+        return redirect(url_for('auth.login_cam', error=error))
+        
+    
+    login_user(user, remember=remember) # Sau khi qua hết thì đăng nhập user
+    print('Đăng nhập khuôn mặt thành công.')
+    return redirect(url_for('main.profile'))  # Chuyển hướng sau khi xác thực bằng khuôn mặt
+
+
---
+++ code/code/project/main.py
@@ -0,0 +1,232 @@
+from flask import Blueprint, render_template, send_from_directory, url_for, current_app, request, Response, send_file, jsonify
+from flask_login import login_required, current_user
+from flask_uploads import UploadSet, IMAGES
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileRequired, FileAllowed
+from wtforms import SubmitField
+from . import db
+from . import imgedit
+from . import videoedit
+from flask_cors import CORS
+from moviepy.editor import VideoFileClip
+
+import base64
+import os
+
+main = Blueprint('main', __name__)
+
+
+@main.route('/')
+def index():
+    return render_template('index.html')
+
+@main.route('/profile')
+@login_required
+def profile():
+    return render_template('profile.html', name=current_user.name)
+
+photos = UploadSet('photos', IMAGES)
+
+class UploadForm(FlaskForm):
+    photo = FileField(
+        validators=[
+            FileAllowed(photos, 'Only images are allowed.'),
+            FileRequired('File field should not be empty.')
+        ]
+    )
+    submit = SubmitField('Upload')
+
+@main.route('/uploads/<filename>')
+def get_file(filename):
+    return send_from_directory(current_app.config['UPLOADED_PHOTOS_DEST'], filename)
+
+@main.route('/takeimage', methods=['POST'])
+def takeimage():
+    # Nhận dữ liệu hình ảnh từ yêu cầu POST
+    data_url = request.json['image']
+
+    # Loại bỏ phần đầu của chuỗi dữ liệu URL (prefix "data:image/jpeg;base64,")
+    img_data = data_url.split(',')[1]
+
+    # Chuyển đổi dữ liệu hình ảnh từ base64 sang binary
+    binary_data = base64.b64decode(img_data)
+
+    # Lưu hình ảnh vào thư mục static/images với tên duy nhất và định dạng jpg
+    image_name = f'image.jpg'
+    image_path = os.path.join(image_name)
+    with open(image_path, 'wb') as f:
+        f.write(binary_data)
+
+    print(f'Image saved at: {image_path}')
+
+    return Response(status=200)
+
+
+@main.route('/RGBtoGray', methods = ['GET', 'POST'])
+@login_required
+def RGBtoGray():
+    form = UploadForm()
+    if form.validate_on_submit():
+        filename = photos.save(form.photo.data)
+        file_url = url_for('main.get_file', filename=filename)
+        gray_filename = imgedit.RGBtoGray(filename, file_url)
+        gray_file_url = url_for('main.get_file', filename=gray_filename)
+    else:
+        file_url = None
+        gray_file_url = None
+    return render_template('imgedit_RGBtoGray.html', form = form, file_url = file_url, gray_file_url = gray_file_url)
+
+@main.route('/face_detection', methods = ['GET', 'POST'])
+@login_required
+def face_detection():
+    form = UploadForm()
+    if form.validate_on_submit():
+        filename = photos.save(form.photo.data)
+        file_url = url_for('main.get_file', filename=filename)
+        face_filename = imgedit.face_detection(filename, file_url)
+        face_file_url = url_for('main.get_file', filename=face_filename)
+    else:
+        file_url = None
+        face_file_url = None
+    return render_template('imgedit_face_detection.html', form = form, file_url = file_url, face_file_url = face_file_url)
+
+@main.route('/crop', methods = ['GET', 'POST'])
+@login_required
+def crop():
+    form = UploadForm()
+    if form.validate_on_submit():
+        x1 = int(request.form.get('x1'))
+        x2 = int(request.form.get('x2'))
+        y1 = int(request.form.get('y1'))
+        y2 = int(request.form.get('y2'))
+        filename = photos.save(form.photo.data)
+        file_url = url_for('main.get_file', filename=filename)
+        face_filename = imgedit.crop(filename, file_url, x1, x2, y1, y2)
+        face_file_url = url_for('main.get_file', filename=face_filename)
+    else:
+        file_url = None
+        face_file_url = None
+    print(file_url)
+    return render_template('imgedit_crop.html', form = form, file_url = file_url, face_file_url = face_file_url)
+
+@main.route('/rotate', methods = ['GET', 'POST'])
+@login_required
+def rotate():
+    form = UploadForm()
+    if form.validate_on_submit():
+        degree = int(request.form.get('degree'))
+        x = int(request.form.get('x'))
+        y = int(request.form.get('y'))
+        filename = photos.save(form.photo.data)
+        file_url = url_for('main.get_file', filename=filename)
+        face_filename = imgedit.rotate(filename, file_url, degree, x, y)
+        face_file_url = url_for('main.get_file', filename=face_filename)
+    else:
+        file_url = None
+        face_file_url = None
+    return render_template('imgedit_rotate.html', form = form, file_url = file_url, face_file_url = face_file_url)
+
+@main.route('/resize', methods = ['GET', 'POST'])
+@login_required
+def resize():
+    form = UploadForm()
+    if form.validate_on_submit():
+        size = int(request.form.get('size'))
+        filename = photos.save(form.photo.data)
+        file_url = url_for('main.get_file', filename=filename)
+        face_filename = imgedit.resize(filename, file_url, size)
+        face_file_url = url_for('main.get_file', filename=face_filename)
+    else:
+        file_url = None
+        face_file_url = None
+    return render_template('imgedit_resize.html', form = form, file_url = file_url, face_file_url = face_file_url)
+
+@main.route('/resize', methods = ['GET', 'POST'])
+@login_required
+def hsv():
+    form = UploadForm()
+    if form.validate_on_submit():
+        h = int(request.form.get('hue'))
+        s = int(request.form.get('saturation'))
+        l = int(request.form.get('light'))
+        filename = photos.save(form.photo.data)
+        file_url = url_for('main.get_file', filename=filename)
+        face_filename = imgedit.hsv(filename, file_url, h, s, l)
+        face_file_url = url_for('main.get_file', filename=face_filename)
+    else:
+        file_url = None
+        face_file_url = None
+    return render_template('imgedit_resize.html', form = form, file_url = file_url, face_file_url = face_file_url)
+
+
+
+
+
+
+
+
+@main.route('/trim_video', methods = ['GET', 'POST'])
+def trim_video():
+    return render_template('trim_video.html')
+
+@main.route('/uploads', methods=['GET'])
+def get_uploaded_videos():
+    uploaded_videos = [filename for filename in os.listdir(current_app.config["UPLOAD_FOLDER"])]
+    return jsonify(uploaded_videos)
+
+@main.route('/clips/<filename>')
+def render_clip(filename):
+    return send_file(os.path.join(current_app.config["UPLOAD_FOLDER"], filename))
+
+@main.route('/playback/<filename>')
+def playback(filename):
+    video_path = os.path.join(current_app.config["UPLOAD_FOLDER"], filename)
+
+    # You may want to add additional security checks here before sending the file
+    # For example, check user authentication, permissions, etc.
+
+    return send_file(video_path)
+
+@main.route('/upload_video', methods=['POST'])
+def upload_video():
+    if not os.path.exists(current_app.config["UPLOAD_FOLDER"]):
+        os.makedirs(current_app.config["UPLOAD_FOLDER"], exist_ok=True)
+
+    try:
+        videofile = request.files['videofile']
+        filepath = os.path.join(current_app.config["UPLOAD_FOLDER"], videofile.filename)
+        videofile.save(filepath)
+        return {"filename": videofile.filename}
+    except FileNotFoundError:
+        return {"error": "File not found"}
+
+@main.route('/edit_video/trim', methods=['POST'])
+def trim_video_edit():
+    try:
+        videofile = request.json['videofile']
+        trim_start = int(request.json['trim_start'])
+        trim_end = int(request.json['trim_end'])
+        trimmed_filename = request.json['trimmed_filename']
+
+        # Append the video extension (e.g., ".mp4") to the filename
+        trimmed_filename_with_extension = f"{trimmed_filename}.mp4"
+
+        edited_videopath = trim_video_function(videofile, trim_start, trim_end, trimmed_filename_with_extension)
+        return {
+            "status": "success",
+            "message": "video edit success",
+            "edited_videopath": edited_videopath
+        }
+    except Exception as e:
+        return {
+            "status": "error",
+            "message": "video edit failure: " + str(e),
+        }
+
+def trim_video_function(videofile, start_time, end_time, trimmed_filename):
+    clip = VideoFileClip(os.path.join(current_app.config["UPLOAD_FOLDER"], videofile))
+    trimmed_filename_with_extension = f"{trimmed_filename}"
+    trimpath = os.path.join(current_app.config["UPLOAD_FOLDER"], trimmed_filename_with_extension)
+    trimmed_clip = clip.subclip(start_time, end_time)
+    trimmed_clip.write_videofile(trimpath)
+    return trimpath
