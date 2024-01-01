4c316ceb770b4a6f3370a21ac07ca0a2ab7ea582
Nidaime-chan <104910191+megashinyabsol359@users.noreply.github.com>
2024-01-01 19:49:20 +0700

    Update videoedit.py

--- code/project/videoedit.py
+++ code/project/videoedit.py
@@ -1,7 +1,6 @@
-from moviepy.editor import VideoFileClip
+from moviepy.editor import VideoFileClip, concatenate_videoclips
 import os
 from flask import current_app
-
 
 def trim_video_function(videofile, start_time, end_time, trimmed_filename):
     clip = VideoFileClip(os.path.join(current_app.config["UPLOAD_FOLDER"], videofile))
@@ -10,3 +9,16 @@ def trim_video_function(videofile, start_time, end_time, trimmed_filename):
     trimmed_clip = clip.subclip(start_time, end_time)
     trimmed_clip.write_videofile(trimpath)
     return trimpath
+
+def merge_video_function(video1_path, video2_path):
+    clip1 = VideoFileClip(video1_path)
+    clip2 = VideoFileClip(video2_path)
+
+    # Generate a unique filename for the merged video
+    merged_filename_with_extension = f"merged_video.mp4"
+    merged_filepath = os.path.join(current_app.config["UPLOAD_FOLDER"], merged_filename_with_extension)
+
+    final_clip = concatenate_videoclips([clip1, clip2])
+    final_clip.write_videofile(merged_filepath, codec="libx264", audio_codec="aac", temp_audiofile="temp-audio.m4a", remove_temp=True, fps=24, threads=4)
+
+    return merged_filepath
